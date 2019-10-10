34a77bab0ee55c465623990ecd11b5fe553be313
Chanthosh Sivanandam <chan1914@edu.ucl.dk>
2019-10-10 17:04:52 +0200

    Del 1 færdig

--- Lufthavn/Startup.cs
+++ Lufthavn/Startup.cs
@@ -18,7 +18,7 @@ namespace Lufthavn
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
